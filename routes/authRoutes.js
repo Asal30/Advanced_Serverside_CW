070228116f0361685b4ce319d34dac3690fcd213
Asal Samarasinghe <asalsamarasinghe30@gmail.com>
2025-04-10 16:10:49 +0530

    added authenticator

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { register, login, generateApiKey } from "../controllers/authController.js";
+import { authenticate } from "../authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.post("/generate-api-key", generateApiKey);
+router.post("/generate-api-key", authenticate, generateApiKey);
 
 export default router;
